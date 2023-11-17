3018e1acae5c9252f7d2bbc614c2f62746f5d0ad
Puru <puru.dore@gmail.com>
2023-11-17 08:44:15 -0800

    prod and dev martech redirects

--- libs/martech/martech.js
+++ libs/martech/martech.js
@@ -90,8 +90,8 @@ const getDtmLib = (env) => ({
   edgeConfigId: env.consumer?.edgeConfigId || env.edgeConfigId,
   url:
     env.name === 'prod'
-      ? env.consumer?.marTechUrl || 'https://assets.adobedtm.com/d4d114c60e50/a0e989131fd5/launch-5dd5dd2177e6.min.js'
-      : env.consumer?.marTechUrl || '/drafts/adobedtm/martech.js',
+      ? env.consumer?.marTechUrl || '/drafts/adobedtm/martech-prod.js'
+      : env.consumer?.marTechUrl || '/drafts/adobedtm/martech-dev.js',
 });
 
 export default async function init({ persEnabled = false, persManifests }) {
